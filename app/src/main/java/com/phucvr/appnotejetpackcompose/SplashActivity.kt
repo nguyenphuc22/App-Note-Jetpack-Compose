b9480fe377f142e2f40d5275a5f48d2036af79fb
nguyenphuc22 <59957741+nguyenphuc22@users.noreply.github.com>
2022-10-16 20:05:10 +0700

    Change Some Splash Activity

--- app/src/main/java/com/phucvr/appnotejetpackcompose/SplashActivity.kt
+++ app/src/main/java/com/phucvr/appnotejetpackcompose/SplashActivity.kt
@@ -2,61 +2,52 @@ package com.phucvr.appnotejetpackcompose
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.phucvr.appnotejetpackcompose.ui.theme.AppNoteJetpackComposeTheme
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(color = MaterialTheme.colors.background) {
-                IconPlash()
-
+            AppNoteJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    IconSplash()
+                }
             }
         }
 
-        lifecycleScope.launch {
+        lifecycleScope.launch(Dispatchers.Main) {
             delay(2000)
-            withContext(Dispatchers.Main) {
-                this@SplashActivity.startActivity(
-                    Intent(this@SplashActivity.application,MainActivity::class.java))
-            }
+            val intent = Intent(this@SplashActivity,
+                MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-@Preview()
-fun IconPlash() {
+fun IconSplash() {
     AppNoteJetpackComposeTheme {
-        // A surface container using the 'background' color from the theme
-        Column(modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally)
-        {
-            Image(painter = painterResource(id = R.drawable.logo),
-                contentDescription ="Logo Image" )
-        }
+        Image(painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo")
     }
 }
