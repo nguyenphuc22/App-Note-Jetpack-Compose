7e4b62c920c21da464ef7ffca931b33e5a5a113d
nguyenphuc22 <59957741+nguyenphuc22@users.noreply.github.com>
2022-12-26 21:42:38 +0700

    Update MainViewModel.kt

--- app/src/main/java/com/phucvr/appnotejetpackcompose/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/phucvr/appnotejetpackcompose/viewmodel/MainViewModel.kt
@@ -1,8 +1,29 @@
 package com.phucvr.appnotejetpackcompose.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.phucvr.appnotejetpackcompose.data.repository.Repository
+import com.phucvr.appnotejetpackcompose.model.NoteModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: Repository) : ViewModel() {
+    val notesNotInTrash: LiveData<List<NoteModel>> by lazy {
+        repository.getAllNotesNotInTrash()
+    }
 
+    fun onCreateNewNoteClick() {
+        // TODO - Open SaveNoteScreen
+    }
+
+    fun onNoteClick(note: NoteModel) {
+        // TODO - Open SaveNoteScreen in Edit mode
+    }
+
+    fun onNoteCheckedChange(note: NoteModel) {
+        viewModelScope.launch(Dispatchers.Default) {
+            repository.insertNote(note)
+        }
+    }
 }
